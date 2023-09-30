a998b82d9680d62584cac2ad00d95d0adf106156
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-09-30 15:27:11 -0300

    Conexão com o banco

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-const express = require('express')
+"use strict"
 
-const app = express()
-const PORT = 4000
+const express = require("express");
+// const cors = require("cors");
+const mysql = require('mysql2')
+
+const PORT = 4000;
+const DB_URL = "mysql://cb56ecbzlkb4mnwsmulo:pscale_pw_4IjntQJQHnGhwpmgqnjwgJyJRmFYWwBfOBSXEt3gbhV@aws.connect.psdb.cloud/api_2?ssl={\"rejectUnauthorized\":true}";
+
+const app = express();
+// app.use(
+//     cors({ origin: ['https://api-2-back.vercel.app'] })
+// );
 
 app.listen(
     PORT,
@@ -19,8 +28,22 @@ app.get(
 
 app.get(
     '/teste',
-    function(req, res) {
-        res.send('Rota teste');
+    async function(req, res) {        
+        const dbConn = mysql.createConnection(DB_URL);
+
+        dbConn.query(
+            'SHOW TABLES',
+            function(err, rows, fields) {
+                if(err) {
+                    throw err;
+                }
+            
+                res.send(rows);
+                console.log(rows);
+            }
+        );
+
+        dbConn.end();
     }
 );
 
