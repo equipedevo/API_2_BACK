4ae2487809e81e800d8149afe5c60ed506811570
Pedro Augusto <pedroaugdev@gmail.com>
2023-10-03 20:43:23 -0300

    T06 - Adicionei as funcoes cadastro, att senha e deletar do funcionario

--- src/rotas/Funcionario.js
+++ src/rotas/Funcionario.js
@@ -4,43 +4,72 @@ const express = require('express');
 const router = express.Router();
 const { CreateConnection, EndConnection } = require('../connection');
 
-async function Cadastro(req, res) {
-    const dbConn = CreateConnection();
-    dbConn.query(
-        `INSERT INTO Funcionario(fun_nome, fun_funcao, fun_email, fun_celular, fun_senha, emp_cod) VALUES('${req.post.nome}', '${req.post.funcao}','${req.post.email}','${req.post.celular}', '${req.post.senha}', ${req.post.emp_cod})`,
-        function(err, rows, fields) {
-            if(err) {
-                throw err;
-            }
+router.post(
+    '/cadastro',
+    function(req, res){
+        const dbConn = CreateConnection();
+        dbConn.query(
+            `INSERT INTO Funcionario(fun_nome, fun_funcao, fun_email, fun_celular, fun_senha, car_cod, emp_cod) VALUES('${req.post.nome}', '${req.post.funcao}','${req.post.email}','${req.post.celular}', '${req.post.senha}', ${req.post.cargo}, ${req.post.emp_cod});`,
+            function(err, rows, fields) {
+                if(err) {
+                    throw err;
+                }
         
-            res.status(200).send();
-            console.log(rows);
-        }
-    );
+                res.status(200).send();
+                console.log(rows);
+            }
+        );
 
-    EndConnection(dbConn);
-}
-
-router.post('/cadastro', Cadastro);
+        EndConnection(dbConn);
+    }
+);
 
 router.post(
     '/login',
     function(req, res) {
-        throw("Login da empresa não implementado");
+        throw("Login do funcionário não implementado");
     }
 );
 
 router.post(
     '/atualizarsenha',
     function(req, res) {
-        throw("Atualizar Senha da empresa não implementado");
+        const dbConn = CreateConnection();
+        dbConn.query(
+            `UPDATE Funcionario SET fun_senha = '${req.post.senha}' WHERE fun_cod = ${codigo};`,
+            function(err, rows, fields){
+                if(err) {
+                    throw err;
+                }
+
+                res.status(200).send();
+                console.log(rows);
+            }
+        );
+
+        EndConnection(dbConn);
+
     }
 );
 
 router.post(
     '/deletar',
     function(req, res) {
-        throw("Deletar da empresa não implementado");
+        const dbConn = CreateConnection();
+        dbConn.query(
+            `DELETE FROM Funcionario WHERE fun_cod = ${req.post.codigo};`,
+            function(err, rows, fields){
+                if(err) {
+                    throw err;
+                }
+                
+                res.status(200).send();
+                console.log(rows);
+            }
+        );
+
+        EndConnection(dbConn);
+
     }
 );
 
