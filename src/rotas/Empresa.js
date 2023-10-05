715e3b8d81d43c674b45bf253d72fbc628685286
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2023-10-04 21:16:44 -0300

    Cadastro e login da empresa

--- src/rotas/Empresa.js
+++ src/rotas/Empresa.js
@@ -3,32 +3,96 @@
 const express = require('express');
 const router = express.Router();
 
+const { CreateConnection, EndConnection } = require('../connection');
+const { HashText, TextHashCompare } = require('../bcrypt')
+
 router.post(
     '/cadastro',
     function(req, res) {
-        throw("Cadastro da empresa não implementado");
+        const nomeCompleto = req.body.nomeCompleto;
+        const cnpj = req.body.cnpj;
+        const senha = HashText(req.body.senha);
+        const email = req.body.email;
+
+        const dbConn = createConnection();
+        dbConn.query(
+            `insert into Empresa('emp_nome', 'emp_cnpj', 'emp_senha', 'emp_email') values('${nomeCompleto}', '${cnpj}', '${senha}', '${email}')`,
+            function(err, result, fields) {
+                if(err) {
+                    res.status(500).send(err);
+                    return;
+                }
+                res.status(200).send(result);
+            }
+        )
+        EndConnection(dbConn);
     }
 );
 
 router.post(
     '/login',
     function(req, res) {
-        throw("Login da empresa não implementado");
+        const dbConn = CreateConnection();
+        dbConn.query(
+            `select emp_senha from Empresa where emp_email = '${req.body.email}'`,
+            function(err, result, fields) {
+                if(err) {
+                    res.status(500).send(err);
+                    return;
+                }
+
+                if(!result) {
+                    res.status(200).send("Não existe uma empresa com esse email no banco de dados.");
+                    return;
+                }
+
+                TextHashCompare(
+                    req.body.senha,
+                    result[0].emp_senha,
+                    function(result) {
+                        res.status(200).send(result);
+                    }
+                )
+            }
+        )
+        EndConnection(dbConn);
     }
 );
 
-router.post(
-    '/atualizarsenha',
-    function(req, res) {
-        throw("Atualizar Senha da empresa não implementado");
-    }
-);
+// router.post(
+//     '/atualizarsenha',
+//     function(req, res) {
+//         const dbConn = createConnection();
+//         dbConn.query(
+//             `Atualizar Senha da empresa não implementado`,
+//             function(err, result, fields) {
+//                 if(err) {
+//                     res.status(500).send(err);
+//                     return;
+//                 }
+//                 res.status(200).send(result);
+//             }
+//         )
+//         EndConnection(dbConn);
+//     }
+// );
 
-router.post(
-    '/deletar',
-    function(req, res) {
-        throw("Deletar da empresa não implementado");
-    }
-);
+// router.post(
+//     '/deletar',
+//     function(req, res) {
+//         const dbConn = createConnection();
+//         dbConn.query(
+//             `Deletar da empresa não implementado`,
+//             function(err, result, fields) {
+//                 if(err) {
+//                     res.status(500).send(err);
+//                     return;
+//                 }
+//                 res.status(200).send(result);
+//             }
+//         )
+//         EndConnection(dbConn);
+//     }
+// );
 
 module.exports = router;
