497b302c4bf3f854e80b8a402ccb6bcae36e7981
Pastel Caique <caiquepericles.23@gmail.com>
2023-10-11 11:59:14 -0300

    Fiz a query do insert do chamado

--- src/rotas/Chamado.js
+++ src/rotas/Chamado.js
@@ -25,20 +25,29 @@ router.get(
     }
 );
 
+
 router.post(
     '/cadastro',
     function (req, res) {
-        const 
+        const desc = req.body.desc
+        const dataInicio = "2004-11-23 08:24:89"
+        const local = req.body.local
+        const titulo = req.body.titulo
+        const codFun = req.body.codFun
+        const codEmp = req.body.codEmp
+
         const dbConn = CreateConnection();
         dbConn.query(
-            `select * from Chamado`,
+            `Insert into Chamado(cha_desc, cha_dataInicio, cha_local, cha_titulo, fun_cod, sta_cod, cha_prioridade, ser_cod, emp_cod) 
+            values ('${desc}', '${dataInicio}', '${local}', '${titulo}','${codFun}', 1, 2, 1, '${codEmp}')`,
             function (err, rows, fields) {
                 if (err) {
-                    res.status(500).send(err);
+                    res.status(500).json({ msg: err });
                     return;
                 }
 
-                res.status(200).send(rows);
+                rres.status(200).json({ msg: `Chamada iniciado com sucesso` });
+                EndConnection(dbConn);
             }
         );
 
