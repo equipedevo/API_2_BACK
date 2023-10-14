dd221cfb19694434a35f4ec3552a47703b09fdce
Pastel Caique <caiquepericles.23@gmail.com>
2023-10-14 18:29:11 -0300

    Coloquei o cha_prioridade

--- src/rotas/Chamado.js
+++ src/rotas/Chamado.js
@@ -10,7 +10,7 @@ router.get(
     function (req, res) {
         const dbConn = CreateConnection(req.query.dev);
         dbConn.query(
-            `select c.cha_cod, c.cha_desc, c.cha_dataInicio, c.cha_dataFim, c.cha_local, c.cha_titulo, f.fun_nome, s.sta_nome, fu.fun_nome Tecnico, se.ser_nome from Chamado c inner join Funcionario f on c.fun_cod = f.fun_cod left join Funcionario fu on c.tec_cod = fu.fun_cod inner join Status s on c.sta_cod = s.sta_cod inner join Tipo_Servico se on c.ser_cod = se.ser_cod;`,
+            `select c.cha_cod, c.cha_desc, c.cha_dataInicio, c.cha_dataFim, c.cha_local, c.cha_titulo, c.cha_prioridade, f.fun_nome, s.sta_nome, fu.fun_nome Tecnico, se.ser_nome from Chamado c inner join Funcionario f on c.fun_cod = f.fun_cod left join Funcionario fu on c.tec_cod = fu.fun_cod inner join Status s on c.sta_cod = s.sta_cod inner join Tipo_Servico se on c.ser_cod = se.ser_cod;`,
             function (err, result, fields) {
                 if (err) {
                     res.status(500).json({ msg: err });
