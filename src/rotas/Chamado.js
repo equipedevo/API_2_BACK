4085b32b648137993f633486ab5e2e3f79fd65dd
Pastel Caique <caiquepericles.23@gmail.com>
2023-10-19 08:02:21 -0300

    Adicionei a variavel codEmp no getTodos no chamado

--- src/rotas/Chamado.js
+++ src/rotas/Chamado.js
@@ -8,10 +8,10 @@ const { CreateConnection, EndConnection } = require('../connection');
 router.post(
     '/getTodos',
     function (req, res) {
+        const codEmp = req.body.codEmp
         const dbConn = CreateConnection(req.query.dev);
         dbConn.query(
-            `
-            select c.cha_cod, c.cha_desc, c.cha_dataInicio, c.cha_dataFim, c.cha_local, c.cha_titulo, c.cha_prioridade, f.fun_nome, s.sta_nome, fu.fun_nome tecnico, se.ser_nome from Chamado c inner join Funcionario f on c.fun_cod = f.fun_cod left join Funcionario fu on c.tec_cod = fu.fun_cod inner join Status s on c.sta_cod = s.sta_cod inner join Tipo_Servico se on c.ser_cod = se.ser_cod inner join Empresa e on c.emp_cod = e.emp_cod where e.emp_cod = ${empCod};`,
+            `select c.cha_cod, c.cha_desc, c.cha_dataInicio, c.cha_dataFim, c.cha_local, c.cha_titulo, c.cha_prioridade, f.fun_nome, s.sta_nome, fu.fun_nome tecnico, se.ser_nome from Chamado c inner join Funcionario f on c.fun_cod = f.fun_cod left join Funcionario fu on c.tec_cod = fu.fun_cod inner join Status s on c.sta_cod = s.sta_cod inner join Tipo_Servico se on c.ser_cod = se.ser_cod inner join Empresa e on c.emp_cod = e.emp_cod where e.emp_cod = ${empCod};`,
             function (err, result, fields) {
                 if (err) {
                     res.status(500).json({ msg: err });
