db7ad28f114537e89f8957fe5bc084379e312779
Pastel Caique <caiquepericles.23@gmail.com>
2023-11-03 20:32:00 -0300

    Tirei o campo de data do getComFiltro do chamado

--- src/rotas/Chamado.js
+++ src/rotas/Chamado.js
@@ -163,7 +163,7 @@ router.post(
         const emp_cod = req.body.emp_cod
 
         const priori = req.body.priori
-        const data = req.body.data
+        // const data = req.body.data
         const func = req.body.func
         const status = req.body.status
         const tipo = req.body.tipo
@@ -171,14 +171,14 @@ router.post(
         //Realização das queries
         const Qprio = (priori != "") ? `c.cha_prioridade = ${priori}, ` : "" // Verifico se o campo está nulo, se não tiver, 
         //é criado um where buscando esse campo, se for nulo o where também vai ser nulo.
-        const Qdata = (data != "") ? `c.cha_dataInicio = '${data}' and ` : ""
+        // const Qdata = (data != "") ? `c.cha_dataInicio = '${data}' and ` : ""
         const Qfunc = (func != "") ? `f.fun_nome = '${func}' and ` : ""
         const Qsta = (status != "") ? `s.sta_nome = '${status}' and ` : ""
         const Qtip = (tipo != "") ? `se.ser_nome = ${tipo} and ` : ""
 
         const dbConn = CreateConnection(req.query.dev);
         dbConn.query(
-            `select c.cha_cod, c.cha_desc, c.cha_dataInicio, c.cha_dataFim, c.cha_local, c.cha_titulo, c.cha_prioridade, f.fun_nome, s.sta_nome, tec.fun_nome tecnico, se.ser_nome from Chamado c inner join Funcionario f on c.fun_cod = f.fun_cod left join Funcionario tec on c.tec_cod = tec.fun_cod inner join Status s on c.sta_cod = s.sta_cod inner join Tipo_Servico se on c.ser_cod = se.ser_cod inner join Empresa e on c.emp_cod = e.emp_cod where ${Qprio}${Qdata}${Qfunc}${Qsta}${Qtip}e.emp_cod = ${emp_cod} order by cha_dataInicio DESC`,
+            `select c.cha_cod, c.cha_desc, c.cha_dataInicio, c.cha_dataFim, c.cha_local, c.cha_titulo, c.cha_prioridade, f.fun_nome, s.sta_nome, tec.fun_nome tecnico, se.ser_nome from Chamado c inner join Funcionario f on c.fun_cod = f.fun_cod left join Funcionario tec on c.tec_cod = tec.fun_cod inner join Status s on c.sta_cod = s.sta_cod inner join Tipo_Servico se on c.ser_cod = se.ser_cod inner join Empresa e on c.emp_cod = e.emp_cod where ${Qprio}${Qfunc}${Qsta}${Qtip}e.emp_cod = ${emp_cod} order by cha_dataInicio DESC`,
             function (err, result, fields) {
                 if (err) {
                     res.status(500).json({ msg: err });
